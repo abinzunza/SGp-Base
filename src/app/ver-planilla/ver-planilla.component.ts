81b2844d18e75966da026f410b0df8faa9ac2649
CristianRojass <31246607+CristianRojass@users.noreply.github.com>
2017-10-14 15:54:48 -0300

    Update ver-planilla.component.ts

--- src/app/ver-planilla/ver-planilla.component.ts
+++ src/app/ver-planilla/ver-planilla.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router'; 
-import { TurnoService } from '../servicios/turno.service';
+import { WebService } from '../servicios/web.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-verPlanilla',
   templateUrl: './ver-planilla.component.html',
   styleUrls: ['./ver-planilla.component.css'],
-  providers: [TurnoService]
+  providers: [WebService]
 })
 
 export class VerPlanillaComponent implements OnInit {
 
-	empleados = {};
+	funcionarios = {};
 	planilla = null;
 	diaSeleccionado = -1;
 	turnoSeleccionado = -1;
 	diasSemana = [];
 	turnosModal:any[] = [];
 
-	constructor(private turnoService:TurnoService, private route: ActivatedRoute, private modalService:NgbModal){}
+	constructor(private webService:WebService, private route: ActivatedRoute, private modalService:NgbModal){}
 	
 	ngOnInit(){
 		this.diasSemana = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo'];
-		this.turnoService.obtenerEmpleados()
-			.subscribe(resEmpleados => resEmpleados.forEach(elemento => this.empleados[elemento._id] = {nombre:elemento.nombre,cargo:elemento.cargo}));
-		this.route.params.subscribe(params => this.turnoService.obtenerPlanilla(new Date(params['id'])).subscribe(resPlanilla => this.planilla = resPlanilla));
+		this.webService.obtenerFuncionarios()
+			.subscribe(resFuncionarios => resFuncionarios.forEach(elemento => this.funcionarios[elemento._id] = {nombre:elemento.nombre,cargo:elemento.cargo}));
+		this.route.params.subscribe(params => this.webService.obtenerPlanilla(new Date(params['id'])).subscribe(resPlanilla => this.planilla = resPlanilla));
 	}
 
 	detalleTurno(dia,turno,modal){
@@ -36,8 +36,8 @@ export class VerPlanillaComponent implements OnInit {
 			this.turnoSeleccionado = turno;
 			for(let i = 0;i<turnos.length;++i) {
 				this.turnosModal.push({
-					nombre:this.empleados[turnos[i].empleado].nombre,
-					cargo:this.empleados[turnos[i].empleado].cargo,
+					nombre:this.funcionarios[turnos[i].funcionario].nombre,
+					cargo:this.funcionarios[turnos[i].funcionario].cargo,
 					inicio:(turnos[i].inicio+8)+":00",
 					fin:(turnos[i].inicio+turnos[i].duracion+8)+":00",
 					idx:Number(this.planilla.dias[dia].turnos.indexOf(turnos[i]))
