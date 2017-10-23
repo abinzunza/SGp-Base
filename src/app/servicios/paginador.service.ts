36a00661517255582c86acbd409c0f8be63761f9
CristianRojass <31246607+CristianRojass@users.noreply.github.com>
2017-10-22 22:17:39 -0300

    Update paginador.service.ts

--- src/app/servicios/paginador.service.ts
+++ src/app/servicios/paginador.service.ts
@@ -2,16 +2,16 @@ import * as _ from 'underscore';
 
 export class PaginadorService {
     obtenerPaginador(totalItems: number, paginaActual: number = 1, tamanoPagina: number = 5) {
-        // calculate total pages
+        // Calcula el total de páginas
         let paginasTotales = Math.ceil(totalItems / tamanoPagina);
 
         let paginaInicial: number, paginaFinal: number;
         if (paginasTotales <= 10) {
-            // less than 10 total pages so show all
+            // Si son 10 o menos páginas se muestran todas
             paginaInicial = 1;
             paginaFinal = paginasTotales;
         } else {
-            // more than 10 total pages so calculate start and end pages
+            // Si son más de 10 páginas se calcula la primera y última página
             if (paginaActual <= 6) {
                 paginaInicial = 1;
                 paginaFinal = 10;
@@ -24,14 +24,14 @@ export class PaginadorService {
             }
         }
 
-        // calculate start and end item indexes
+        // Calcula el inicio y fin en los indices para item's
         let indiceInicial = (paginaActual - 1) * tamanoPagina;
         let indiceFinal = Math.min(indiceInicial + tamanoPagina - 1, totalItems - 1);
 
-        // create an array of pages to ng-repeat in the pager control
+        // Crea un array de páginas para el ngFor
         let paginas = _.range(paginaInicial, paginaFinal + 1);
 
-        // return object with all pager properties required by the view
+        // Retorna un objeto con todas las propiedades de página requeridas por las vistas
         return {
             totalItems: totalItems,
             paginaActual: paginaActual,
