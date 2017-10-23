acfe3500e5d8fd33ddb78aa7e960bb32844eeb8c
CristianRojass <31246607+CristianRojass@users.noreply.github.com>
2017-10-22 22:09:39 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { LOCALE_ID } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MostrarFuncionariosComponent } from './funcionarios/mostrar-funcionarios.component';
+
+import { CrearPlanillaDeactivateGuard } from './guard/crear-planilla-deactivate.guard';
+import { PaginadorService } from './servicios/paginador.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MostrarFuncionariosComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    NgbModule.forRoot(),
+    AppRoutingModule
+  ],
+  providers: [{ provide: LOCALE_ID, useValue: "es-ES" },CrearPlanillaDeactivateGuard,PaginadorService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
