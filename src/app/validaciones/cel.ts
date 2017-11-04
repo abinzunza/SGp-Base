3c6e431d9694705f36124af860d6def1cda1062d
CristianRojass <cristian.rojas@alumnos.uv.cl>
2017-11-04 04:35:19 -0300

    Rut sin puntos, Telefono con once digitos

--- src/app/validaciones/cel.ts
+++ src/app/validaciones/cel.ts
@@ -3,7 +3,7 @@ import { AbstractControl } from '@angular/forms';
 export class CelValidator {
 	
 	static verificarFormatoCel(c:AbstractControl){
-		let regExpCel = /^[0-9]{8}$/;
+		let regExpCel = /^[0-9]{11}$/;
 		if(regExpCel.test(c.value))return null;
 		return {formatoIncorrecto:true};
 	}
